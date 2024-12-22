5bce95b81431788d2ed2e4a16602c85479dfcc25
CH.Vidya Prasuna <155738724+vidya100804@users.noreply.github.com>
2024-12-22 14:22:29 +0530

    Add files via upload

---
+++ binarySearch.java
@@ -0,0 +1,26 @@
+package SEARCHINGS;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] arr = {10, 19, 24, 36, 45, 56, 77, 89};
+        int target = 77;
+        int result = binary(arr, target);
+        System.out.println("Found At Index: " + result);
+    }
+
+    static int binary(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1; 
+    }
+}
